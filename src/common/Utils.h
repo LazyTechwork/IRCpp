dfe7155542630b816e4f6c387fad174375773906
LazyTechwork <lazytechwork@gmail.com>
2021-11-05 01:59:08 +0300

    Errors hotfix

--- src/common/Utils.h
+++ src/common/Utils.h
@@ -8,5 +8,6 @@
 namespace Utils {
     std::vector<std::string> SplitString(std::string &str, char delimiter = ' ');
 
+    template <typename Iter>
     std::string JoinString(Iter begin, Iter end, const std::string &separator);
 }
